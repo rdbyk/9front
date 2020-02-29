4be90ccd7cac1b6f80853c982d1c58687cb6932c
Dirk Reusch <rdbyk@users.noreply.github.com>
2020-02-29 17:51:04 +0100

    atan2: fix result for signed zero and infinite args

--- sys/src/libc/port/atan2.c
+++ sys/src/libc/port/atan2.c
@@ -9,16 +9,21 @@ double
 atan2(double arg1, double arg2)
 {
 
-	if(arg1+arg2 == arg1) {
-		if(arg1 >= 0)
-			return PIO2;
-		return -PIO2;
+	if(arg1+arg2 == arg1 || arg1-arg2 == arg1) {
+		if(arg1 == 0) {
+			if (signbit(arg2))
+				return copysign(PI, arg1);
+			return arg1;
+		}
+		if(isInf(arg2, 0)) {
+			if(arg2 < 0)
+				return copysign(3*PI/4, arg1);
+			return copysign(PI/4, arg1);
+		}
+		return copysign(PIO2, arg1);
 	}
 	arg1 = atan(arg1/arg2);
-	if(arg2 < 0) {
-		if(arg1 <= 0)
-			return arg1 + PI;
-		return arg1 - PI;
-	}
+	if(arg2 < 0)
+		return arg1 - copysign(PI, arg1);
 	return arg1;
 }
