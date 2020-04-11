c1830109bd7cc277bbd4b1595bd1f37bcf7120b0
Dirk Reusch <rdbyk@users.noreply.github.com>
2020-04-11 16:52:24 +0200

    ap/math/hypot.c: update from libc sources

--- sys/src/ape/lib/ap/math/hypot.c
+++ sys/src/ape/lib/ap/math/hypot.c
@@ -1,29 +1,44 @@
-#include <math.h>
 /*
- * sqrt(a^2 + b^2)
- *	(but carefully)
+ * hypot -- sqrt(p*p+q*q), but overflows only if the result does.
+ * See Cleve Moler and Donald Morrison,
+ * ``Replacing Square Roots by Pythagorean Sums,''
+ * IBM Journal of Research and Development,
+ * Vol. 27, Number 6, pp. 577-581, Nov. 1983
  */
 
-double
-hypot(double a, double b)
-{
-	double t;
+#include <math.h>
 
-	if(a < 0)
-		a = -a;
-	if(b < 0)
-		b = -b;
-	if(a > b) {
-		t = a;
-		a = b;
-		b = t;
+double
+hypot(double p, double q)
+{
+	double r, s, pfac;
+
+	if(p < 0)
+		p = -p;
+	if(q < 0)
+		q = -q;
+	if(p < q) {
+		r = p;
+		p = q;
+		q = r;
 	}
-	if(b == 0) 
+	if(p == 0)
 		return 0;
-	a /= b;
-	/*
-	 * pathological overflow possible
-	 * in the next line.
-	 */
-	return b * sqrt(1 + a*a);
+	if(isInf(p,1) || isInf(q,1))
+		return Inf(1);
+	if(isNaN(p + q))
+		return p + q;
+	pfac = p;
+	r = q = q/p;
+	p = 1;
+	for(;;) {
+		r *= r;
+		s = r+4;
+		if(s == 4)
+			return p*pfac;
+		r /= s;
+		p += 2*r*p;
+		q *= r;
+		r = q/p;
+	}
 }
